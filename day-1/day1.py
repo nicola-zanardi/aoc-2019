c92ffddce8dee1dae29577218de2c0677914e1e1
nicola.zanardi <nicola.zanardi@energy21.com>
2019-12-02 11:25:28 +0100

    day 1 - part 2

--- day-1/day1.py
+++ day-1/day1.py
@@ -1,15 +1,39 @@
 import math
 
+
 def fuel_calculator(mass):
-    return math.floor(mass/3) - 2
+    fuel = math.floor(mass / 3) - 2
+    if fuel < 0:
+        return 0
+    return fuel
+
 
 def part1():
     total_fuel = 0
-    with open('input.txt') as data:
+    with open("input.txt") as data:
         for module in data:
             total_fuel = total_fuel + fuel_calculator(int(module))
-            
+
     return total_fuel
+
+
+def fuel_calculator_improved(mass):
+    fuel = fuel_calculator(mass)
+    if fuel == 0:
+        return fuel
+    return fuel + fuel_calculator_improved(fuel)
+
+
+def part2():
+    total_fuel = 0
+    with open("input.txt") as data:
+        for module in data:
+            total_fuel = total_fuel + fuel_calculator_improved(int(module))
+
+    return total_fuel
+
 
 print("PART 1")
 print(f"Total fuel needed: {part1()}")
+print("PART 2")
+print(f"Total fuel needed: {part2()}")
--- day-1/test_day1.py
+++ day-1/test_day1.py
@@ -1,4 +1,4 @@
-from day1 import fuel_calculator
+from day1 import fuel_calculator, fuel_calculator_improved
 
 
 def test_part1_1():
@@ -15,3 +15,15 @@ def test_part1_3():
 
 def test_part1_3():
     assert fuel_calculator(100756) == 33583
+
+
+def test_part2_1():
+    assert fuel_calculator_improved(14) == 2
+
+
+def test_part2_2():
+    assert fuel_calculator_improved(1969) == 966
+
+
+def test_part2_3():
+    assert fuel_calculator_improved(100756) == 50346
