ae438e59927593db56d6d0abdf657593a284ed26
nicola.zanardi <nicola.zanardi@energy21.com>
2019-12-03 13:18:28 +0100

    day3 - part 2

--- day-3/day3.py
+++ day-3/day3.py
@@ -2,6 +2,8 @@
 # Starting point should be x=0 y=0
 # After, let's follow the carthesian coordinates and increase up-right.
 
+from typing import List
+
 
 def compute_position(movement: str, starting_point):
     direction = movement[0]
@@ -22,11 +24,11 @@ def compute_position(movement: str, starting_point):
     ]
 
 
-def distance(position):
-    return abs(position[0]) + abs(position[1])
+def distance(position, start_position=(0, 0)):
+    return abs(position[0] - start_position[0]) + abs(position[1] - start_position[1])
 
 
-def get_positions_list(movements: str):
+def get_positions_list(movements: str) -> List:
     positions = [(0, 0)]
     movements = movements.split(",")
     for movement in movements:
@@ -45,6 +47,25 @@ def part_1(movements_cable_1, movements_cable_2):
     return min([distance(intersection) for intersection in intersections])
 
 
+def travelled_distance(positions, end):
+    steps = positions[: positions.index(end) + 1]
+    cable_distance = 0
+    for i in range(1, len(steps)):
+        cable_distance += distance(positions[i], positions[i - 1])
+    return cable_distance
+
+
+def part_2(movements_cable_1, movements_cable_2):
+    positions_1 = get_positions_list(movements_cable_1)
+    positions_2 = get_positions_list(movements_cable_2)
+    intersections = find_intersections(positions_1, positions_2)
+    distances = [
+        travelled_distance(positions_1, end) + travelled_distance(positions_2, end)
+        for end in intersections
+    ]
+    return min(distances)
+
+
 if __name__ == "__main__":
     with open("input.txt") as data:
         coordinates = data.readlines()
@@ -52,3 +73,4 @@ if __name__ == "__main__":
     coordinates = [coordinate.strip() for coordinate in coordinates]
 
     print(f"PART 1: {part_1(*coordinates)}")
+    print(f"PART 2: {part_2(*coordinates)}")
--- day-3/test_day3.py
+++ day-3/test_day3.py
@@ -1,4 +1,4 @@
-from day3 import part_1
+from day3 import part_1, part_2
 
 
 def test_part1_1():
@@ -19,5 +19,26 @@ def test_part1_3():
             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
         )
         == 135
+    )
+
+
+def test_part2_1():
+    assert part_2("R8,U5,L5,D3", "U7,R6,D4,L4") == 30
+
+
+def test_part2_2():
+    assert (
+        part_2("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")
+        == 610
+    )
+
+
+def test_part2_2():
+    assert (
+        part_2(
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        )
+        == 410
     )
 
