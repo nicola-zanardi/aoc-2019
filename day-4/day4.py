71ec8506a0e56f8c21441e2920c24bda6e2abf8a
nicola.zanardi <nicola.zanardi@energy21.com>
2019-12-04 09:24:13 +0100

    Code cleanup

--- day-4/day4.py
+++ day-4/day4.py
@@ -1,26 +1,17 @@
-def pw_check(pw):
+def part1(pw):
     pw = list(str(pw))
-    for i in range(1, len(pw)):
-        if pw[i - 1] > pw[i]:
-            return False
-    if len(set(pw)) == len(pw):
+    if pw != sorted(pw) or len(set(pw)) == len(pw):
         return False
-    doubles = [digit for digit in pw if pw.count(digit) > 1]
-    ok_doubles = 0
-    for double in doubles:
-        if pw[pw.index(double)] == pw[pw.index(double) + 1]:
-            # addition for part 2
-            if (
-                len(pw) - 1 == pw.index(double) + 1
-                or pw[pw.index(double) + 1] != pw[pw.index(double) + 2]
-            ):
-                ok_doubles += 1
-    return ok_doubles > 0
+    return True
 
 
-n = 0
-for pw in range(147981, 691423 + 1):
-    if pw_check(pw):
-        n += 1
-print(n)
+def part2(pw):
+    return 2 in [str(pw).count(d) for d in str(pw)]
+
+
+pws = [pw for pw in range(147981, 691423 + 1) if part1(pw)]
+print(f"PART 1: {len(pws)}")
+
+pws_2 = [pw for pw in pws if part2(pw)]
+print(f"PART 2: {len(pws_2)}")
 
